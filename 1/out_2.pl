021a6938f0acfbc7033ac76e50766ef2cf587cba
Fabian <mail@fabianbissmann.de>
2023-12-02 08:26:04 +0100

    add 1_2

--- 1/out_2.pl
+++ 1/out_2.pl
@@ -1,20 +1,36 @@
 #!/usr/bin/perl
+use Syntax::Construct qw(/r);
 use warnings;
 use strict;
 
 my $sum = 0;
+my @digits = ("one", "two", "three", "four", "five",
+              "six", "seven", "eight", "nine");
+
 while (<>) {
   my $first_number;
   my $last_number;
   my $concat_number;
 
-  $first_number = $1 if $_ =~ /[.]?([0-9]).*/;
-  $last_number = $1 if $_ =~ /[.]?[0-9].*([0-9])/;
+  $first_number = $1 if $_ =~ /[.]?([0-9]|one|two|three|four|five|six|seven|eight|nine).*/;
+  $last_number = $1 if $_ =~ /.*([0-9]|one|two|three|four|five|six|seven|eight|nine)/;
 
-  $concat_number = $first_number if defined $first_number;
-  $concat_number .= $last_number if defined $last_number;
+  if (defined $first_number) {
+    for my $i (0 .. $#digits) {
+      my $j = $i + 1;
+      $first_number = $first_number =~ s/$digits[$i]/$j/r;
+    }
+    $concat_number = $first_number;
+  }
+  if (defined $last_number) {
+    for my $i (0 .. $#digits) {
+      my $j = $i + 1;
+      $last_number = $last_number =~ s/$digits[$i]/$j/r;
+    }
+    $concat_number .= $last_number;
+  }
   $concat_number .= $concat_number if length($concat_number) == 1;
-  
+
   $sum += $concat_number;
 }
 
