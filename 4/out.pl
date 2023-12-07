10113561f01b5a9dab6e0e3622a086511994391b
- <>
2023-12-07 17:41:07 +0100

    change 4

--- 4/out.pl
+++ 4/out.pl
@@ -10,11 +10,44 @@ my $winning_number;
 my $current_number;
 
 while (<>) {
-  $winning_number = $1 if $_ =~ /Card \d+:\s((\s*[\d]{,2}\s*){5})/;
-  $current_number = $1 if $_ =~ /Card \d+:\s.*\s(([\d]{,2}\s*){8})/;
+  $winning_number = $1 if $_ =~ /Card\s*\d+:\s((\s*[\d]{,2}\s*){10})/;
+  $current_number = $1 if $_ =~ /Card\s*\d+:\s.*\s\| (([\d]{,2}\s*){25})/;
   
+  my @tmp_w = ();
+  my @tmp_c = ();
   for my $w_num (split(/\s/, $winning_number)) {
-    print "$w_num . ";
+    if (($w_num =~ /\d{,2}/) && ($w_num ne "")) {
+      push(@tmp_w, $w_num);
+    }
   }
-  print "\n";
+  push(@winning_numbers, \@tmp_w);
+  for my $c_num (split(/\s/, $current_number)) {
+    if (($c_num =~ /\d{,2}/) && ($c_num ne "")) {
+      push(@tmp_c, $c_num);
+    }
+  }
+  push(@current_numbers, \@tmp_c);
 }
+
+my $sum = 0;
+my $score = 0;
+my $card_index = 0;
+for my $w_cards (@winning_numbers) {
+  for my $winning_number (@$w_cards) {
+    for my $current_number (@{$current_numbers[$card_index]}) {
+      if ($winning_number == $current_number) {
+        print "winning: $current_number\n";
+        $score++;
+      }
+    }
+  }
+  if ($score > 0) {
+    $score--;
+    $sum += (2**$score);
+    print "2**$score = " . 2**$score . "\n";
+    $score = 0;
+  }
+  $card_index++;
+  print "new card\n";
+}
+print "$sum\n";
