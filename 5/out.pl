68cad3b4057c3c55f8e6a3572ff6e2d57ccf4eaa
Fabian <mail@fabianbissmann.de>
2023-12-16 02:34:50 +0100

    5a done

--- 5/out.pl
+++ 5/out.pl
@@ -12,50 +12,15 @@ my %light_to_temp = ();
 my %temp_to_humid = ();
 my %humid_to_location = ();
 
+my @translated_values = ();
+my $translated_value;
+
 my @seeds;
 my @range_condensed = ();
 
 # mode: 0=init, 1=s2s, 2=s2f, 3=f2w
 #       4=w2l,   5=l2t, 6=t2h, 7=h2l
 my $mode = 0;
-
-# fill_hash \@range_condensed : %range_expanded
-sub fill_hash {
-  my %range_expanded = ();
-  my @range_condensed = @{$_[0]};
-
-  for my $range_line (@range_condensed) {
-    # $range_line[0] = dest
-    # $range_line[1] = src
-    # $range_line[2] = range
-    # %range_expanded{"src"} = dest
-    my @range_line = split(/\s/, $$range_line);
-    for (my $i=0; $i < $range_line[2]; $i++) {
-      my $range_index = $i + $range_line[1];
-      $range_expanded{"$range_index"} = $i + $range_line[0];
-    }
-  }
-  return %range_expanded;
-}
-
-# translate_items \%range_expanded, \@seeds : @translated
-sub translate_items {
-  my %range_expanded = %{$_[0]};
-  my @seeds = @{$_[1]};
-  my @translated = ();
-
-  my $translation = 0;
-
-  for my $seed (@seeds) {
-    if ($range_expanded{$seed}) {
-      $translation = $range_expanded{$seed};
-    } else {
-      $translation = $seed;
-    }
-    print "for seed $seed: $translation .";
-  }
-  print "\n";
-}
 
 # test_in_ranges \@range_condensed, seed : (dest, src, range)
 sub test_in_ranges {
@@ -67,14 +32,16 @@ sub test_in_ranges {
     # $range_line[0] = dest
     # $range_line[1] = src
     # $range_line[2] = range
-    if (($to_translate > $range_line[1]) and ($to_translate < ($range_line[1] + $range_line[2]))) {
+    if (($to_translate >= $range_line[1]) and ($to_translate < ($range_line[1] + $range_line[2]))) {
+      print("$to_translate match in $range_line[0], $range_line[1], $range_line[2]\n");
       return ($range_line[0], $range_line[1], $range_line[2]);
     }
   }
-  return 0;
+  return -1;
 }
 
 while (<>) {
+  print($_ . "\n");
   if ($_ =~ /seeds:/) {
     @seeds = split(/\s/, ($_ =~ s/^seeds: //r));
   }
@@ -88,19 +55,18 @@ while (<>) {
     elsif ($_ =~ /^.$/s) {
       for my $seed (@seeds) {
         my @res = test_in_ranges(\@range_condensed, $seed);
-        if ($res[0]) {
-          print("$seed in range $res[0], $res[1], $res[2].\n");
+        if ($res[0] != -1) {
+          $translated_value = $seed - $res[1] + $res[0];
         } else {
-          print ("$seed in no range\n");
+          $translated_value = $seed + 0;
         }
+        push(@translated_values, $translated_value);
       }
-      #%seed_to_soil = fill_hash(\@range_condensed);
       @range_condensed = ();
       $mode = 0;
-      #print Dumper(\%seed_to_soil);
-      #print Dumper(\@seeds);
-      #translate_items(\%seed_to_soil, \@seeds);
-      exit 0;
+      @seeds = @translated_values;
+      @translated_values = ();
+      print Dumper(\@seeds);
     }
   }
   elsif ((/soil-to-fertilizer/) || ($mode == 2)) {
@@ -111,9 +77,20 @@ while (<>) {
       push(@range_condensed, \$line);
     }
     elsif ($_ =~ /^.$/s) {
-      %soil_to_fertil = fill_hash(\@range_condensed);
+      for my $seed (@seeds) {
+        my @res = test_in_ranges(\@range_condensed, $seed);
+        if ($res[0] != -1) {
+          $translated_value = $seed - $res[1] + $res[0];
+        } else {
+          $translated_value = $seed + 0;
+        }
+        push(@translated_values, $translated_value);
+      }
       @range_condensed = ();
       $mode = 0;
+      @seeds = @translated_values;
+      @translated_values = ();
+      print Dumper(\@seeds);
     }
   }
   elsif ((/fertilizer-to-water/) || ($mode == 3)) {
@@ -124,9 +101,20 @@ while (<>) {
       push(@range_condensed, \$line);
     }
     elsif ($_ =~ /^.$/s) {
-      %fertil_to_water = fill_hash(\@range_condensed);
+      for my $seed (@seeds) {
+        my @res = test_in_ranges(\@range_condensed, $seed);
+        if ($res[0] != -1) {
+          $translated_value = $seed - $res[1] + $res[0];
+        } else {
+          $translated_value = $seed + 0;
+        }
+        push(@translated_values, $translated_value);
+      }
       @range_condensed = ();
       $mode = 0;
+      @seeds = @translated_values;
+      @translated_values = ();
+      print Dumper(\@seeds);
     }
   }
   elsif ((/water-to-light/) || ($mode == 4)) {
@@ -137,9 +125,20 @@ while (<>) {
       push(@range_condensed, \$line);
     }
     elsif ($_ =~ /^.$/s) {
-      %water_to_light = fill_hash(\@range_condensed);
+      for my $seed (@seeds) {
+        my @res = test_in_ranges(\@range_condensed, $seed);
+        if ($res[0] != -1) {
+          $translated_value = $seed - $res[1] + $res[0];
+        } else {
+          $translated_value = $seed + 0;
+        }
+        push(@translated_values, $translated_value);
+      }
       @range_condensed = ();
       $mode = 0;
+      @seeds = @translated_values;
+      @translated_values = ();
+      print Dumper(\@seeds);
     }
   }
   elsif ((/light-to-temperature/) || ($mode == 5)) {
@@ -150,9 +149,20 @@ while (<>) {
       push(@range_condensed, \$line);
     }
     elsif ($_ =~ /^.$/s) {
-      %light_to_temp = fill_hash(\@range_condensed);
+      for my $seed (@seeds) {
+        my @res = test_in_ranges(\@range_condensed, $seed);
+        if ($res[0] != -1) {
+          $translated_value = $seed - $res[1] + $res[0];
+        } else {
+          $translated_value = $seed + 0;
+        }
+        push(@translated_values, $translated_value);
+      }
       @range_condensed = ();
       $mode = 0;
+      @seeds = @translated_values;
+      @translated_values = ();
+      print Dumper(\@seeds);
     }
   }
   elsif ((/temperature-to-humidity/) || ($mode == 6)) {
@@ -163,9 +173,20 @@ while (<>) {
       push(@range_condensed, \$line);
     }
     elsif ($_ =~ /^.$/s) {
-      %temp_to_humid = fill_hash(\@range_condensed);
+      for my $seed (@seeds) {
+        my @res = test_in_ranges(\@range_condensed, $seed);
+        if ($res[0] != -1) {
+          $translated_value = $seed - $res[1] + $res[0];
+        } else {
+          $translated_value = $seed + 0;
+        }
+        push(@translated_values, $translated_value);
+      }
       @range_condensed = ();
       $mode = 0;
+      @seeds = @translated_values;
+      @translated_values = ();
+      print Dumper(\@seeds);
     }
   }
   elsif ((/humidity-to-location/) || ($mode == 7)) {
@@ -176,9 +197,20 @@ while (<>) {
       push(@range_condensed, \$line);
     }
     elsif ($_ =~ /^.$/s) {
-      %humid_to_location = fill_hash(\@range_condensed);
+      for my $seed (@seeds) {
+        my @res = test_in_ranges(\@range_condensed, $seed);
+        if ($res[0] != -1) {
+          $translated_value = $seed - $res[1] + $res[0];
+        } else {
+          $translated_value = $seed + 0;
+        }
+        push(@translated_values, $translated_value);
+      }
       @range_condensed = ();
       $mode = 0;
+      @seeds = @translated_values;
+      @translated_values = ();
     }
   }
 }
+print Dumper(\@seeds);
